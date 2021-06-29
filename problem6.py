0b3ff80315f796630660045cc093ad0a32a718ad
Victor Amonde <amondev00@gmail.com>
2021-06-29 23:08:46 +0300

    Modify problem6.py

--- problem6.py
+++ problem6.py
@@ -9,16 +9,16 @@ def main():
     for i in sent:
         s.append(i)
     # print(s)
-    s1 = list(enumerate(s))
-    # print(s1)
+    s1 = enumerate(s)
+#     print(s1)
     for i in s1:
         if i is not s2:
             s2[i] = 1
         else:
-            s2[i] = s2[i] + s2[i + 1]
-    for i, j in s2:
-        print(i, '->', j)
-        return os.EX_OK
+            s2[i] = s2[i] + 1
+    for k, v in s2:
+        print(k, '->', v)
+    return os.EX_OK
 
 
 if __name__ == "__main__":
