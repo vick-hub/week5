ab24f0ff2f5f64ae687fc9790ed9cc4c98d777cf
vick-hub <amondev00@gmail.com>
2021-07-01 16:51:50 +0300

    Added problem4.py

---
+++ problem4.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import random
+
+
+def main():
+    alphabet = list('ACGT')
+    dna = [random.choice(alphabet) for i in range(100)]
+    dna = ''.join(dna)
+    print(f"DNA: {dna}")
+    rna = ""
+    for i in dna:
+        if i == "A":
+            rna += "U"
+        elif i == "C":
+            rna += "G"
+        elif i == "G":
+            rna += "C"
+        else:
+            rna += "A"
+    print(f"RNA: {rna}")
+    return os.EX_OK
+
+
+if __name__ == "__main__":
+    sys.exit(main())
