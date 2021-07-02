bce8832b99f827dc5cb6b2b3384f7bfd6d06dd0c
vick-hub <amondev00@gmail.com>
2021-07-02 14:12:57 +0300

    Modify problem4

--- problem4.py
+++ problem4.py
@@ -19,7 +19,13 @@ def main():
         else:
             rna += "A"
     print(f"RNA: {rna}")
-    return os.EX_OK
+#    protein_string = ""
+#    for i in range(0, len(rna)-(3+len(rna) % 3), 3):
+#         if rna[i:i+3] == 'STOP':
+#            break
+#        protein_string += rna[i+3]
+#    print("Protein string:", protein_string)
+#    return os.EX_OK
 
 
 if __name__ == "__main__":
