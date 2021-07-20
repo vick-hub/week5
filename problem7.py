d53034d62d090734c57a7d7c048b5fcc5f6717af
vick-hub <amondev00@gmail.com>
2021-07-20 12:00:27 +0300

    modify problem7

--- problem7.py
+++ problem7.py
@@ -15,20 +15,21 @@ def main():
     - rock vs. paper: paper wins because a rock can be wrapped in a paper;
     - rock vs. scissors: rock wins because a rock can crush a pair of scissors;
     - paper vs. scissors: scissors wins because it can cut the paper.
-    - when you type 'done' game is over.""")
+    - when you type 'done' game is over.
+    -r represent rock, p represent paper, s represent scissors and x represent exit.""")
     print('=' * 120)
-    game = random.choice(['rock', 'paper', 'scissors'])
+    game = random.choice(['r', 'p', 's'])
     # print(game)
     selection = input('Enter your selection: ')
-    while selection != 'done':
+    while selection != 'x':
         if game == selection:
             print("draw")
             selection = input('Enter your selection: ')
-        if game == 'rock' and selection == 'paper':
+        if game == 'r' and selection == 'p':
             print("You won!!!")
-        elif game == 'scissors' and selection == 'rock':
+        elif game == 's' and selection == 'r':
             print("You won!!!")
-        elif game == 'paper' and selection == 'scissors':
+        elif game == 'p' and selection == 's':
             print("You won!!!")
         else:
             print("You lost!!!")
